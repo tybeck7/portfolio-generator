eacbe91db1e5667f78121cfc49c52ea0fb144233
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-02-19 20:20:22 -0800

    interacted with inquirer

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer')
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const pageHTML = generatePage(name, github);
 
-  console.log('================');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
